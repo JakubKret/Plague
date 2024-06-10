7647845f3f4314826c19739d3017a1bffaa18163
WojszczykMaciej <wojszczykmaciej@gmail.com>
2024-06-10 22:51:48 +0200

    Add files via upload

--- src/Plague.java
+++ src/Plague.java
@@ -4,16 +4,21 @@ public class Plague {
 
     public static void main(String[] args) throws InterruptedException {
         CombinedVisualizer combinedVisualizer = new CombinedVisualizer();
+        Virus virus = new Virus(combinedVisualizer);
         //combinedVisualizer.display();
 
+        virus.patientZero();
         while (1>0){
-            Thread.sleep(1000);
+            Thread.sleep(1);
             //System.out.println(combinedVisualizer.getPopulation().size());
             //System.out.println(combinedVisualizer.getPopulation().get(0).getPosX()+" "+combinedVisualizer.getPopulation().get(0).getPosY());
+            virus.virusSpread();
             combinedVisualizer.movePopulation();
             //System.out.println(combinedVisualizer.getPopulation().get(0).getPosX()+" "+combinedVisualizer.getPopulation().get(0).getPosY());
+            virus.virusAction();
             combinedVisualizer.refreshVisualization();
             //combinedVisualizer.display();
+
         }
     }
 }
