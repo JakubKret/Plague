18a822211ee96ad989f68481913c573b12c7a4d1
WojszczykMaciej <wojszczykmaciej@gmail.com>
2024-06-10 22:51:01 +0200

    Add files via upload

--- src/Human.java
+++ src/Human.java
@@ -47,26 +47,31 @@ import java.util.Random;
 public class Human {
     private int posX,posY;
     private double health = 40;
-    private double immunity= 0.1;
+    private double immunity = 0.1;
     private boolean isInfected=false;
     private boolean canInfect = false;
     private boolean isDead = false;
-    public Human(int X,int Y){
+    private Tile[][] board;
+    Random rand = new Random();
+    public Human(int X,int Y, Tile[][] board){
         this.posX = X;
         this.posY = Y;
+        this.board = board;
+        this.immunity = rand.nextDouble();
     }
 
     public void Move(){
+        if(!isDead){
         Random rand = new Random();
         int x = rand.nextInt(3);
         x=x-1;
-        if(x==-1 && posX!=0)    posX--;
-        else if(x==1 && posX!=CombinedVisualizer.WIDTH-1)   posX++;
+        if(x==-1 && posX!=0 && board[posX-1][posY].isLand)    posX--;
+        else if(x==1 && posX!=CombinedVisualizer.WIDTH-1 && board[posX+1][posY].isLand)   posX++;
         int y = rand.nextInt(3);
         y=y-1;
-        if(y==-1 && posY!=0)    posY--;
-        else if(y==1 && posY!=CombinedVisualizer.HEIGHT-1)  posY++;
-    }
+        if(y==-1 && posY!=0 && board[posX][posY-1].isLand)    posY--;
+        else if(y==1 && posY!=CombinedVisualizer.HEIGHT-1 && board[posX][posY+1].isLand)  posY++;
+    }}
     public void giveSick()//
     {
         this.isInfected = true;
@@ -83,13 +88,16 @@ public class Human {
     public boolean getIsInfected(){
         return isInfected;
     }
+    public boolean getCanInfect(){return canInfect;}
 
-    public void startToInfect(){this.canInfect = true;}
+    public void setCanInfect(){this.canInfect = true;}
     public double getImmunity(){
         return immunity;
     }
     public void virusEffect(double x){
         if(isInfected){
+            x = x - (immunity/2);
+            if(x <= 0.01){x = 0.01;}
             health = health - x;
         }
         if(health <= 0){
@@ -98,68 +106,3 @@ public class Human {
         }
     }
 }
-
-
-/*import java.util.Random;
-public class Human {
-    static Random rand = new Random();
-    private double health = 40;
-    private static double immunity= rand.nextDouble();
-    public static int posX;
-    public static int posY;
-    public boolean isInfected = false;
-    public boolean canInfect = false;
-    public boolean isDead = false;
-    private Tile[][] board;
-    public Human(int X, int Y,Tile[][] board){
-        this.posX = X;
-        this.posY = Y;
-        this.board = board;
-    }
-//    public void Move(){
-//
-//        int x = rand.nextInt(3);
-//        x=x-1;
-//        if(x==-1 && board[posX-1][posY].isLand)--posX;
-//        else if(x==1 && board[posX+1][posY].isLand)++posX;
-//        int y = rand.nextInt(3);
-//        y=y-1;
-//        if(y==-1 && board[posX][posY-1].isLand)--posY;
-//        else if(y==1 && board[posX][posY+1].isLand)++posY;
-//    }
-    public void Move(){
-        Random rand = new Random();
-        int x = rand.nextInt(3);
-        x=x-1;
-        if(x==-1 && posX!=0)--posX;
-        else if(x==1 && posX!=CombinedVisualizer.WIDTH-1)++posX;
-        int y = rand.nextInt(3);
-        y=y-1;
-        if(y==-1 && posY!=0)--posY;
-        else if(y==1 && posY!=CombinedVisualizer.HEIGHT-1)++posY;
-    }
-    public double getHealth() {
-        return health;
-    }
-    public static int getPosX(){
-        return posX;
-    }
-    public static int getPosY(){
-        return posY;
-    }
-    public void giveSick(){this.isInfected = true;}
-    public void startToInfect(){this.canInfect = true;}
-    public static double getImmunity(){
-        return immunity;
-    }
-    public void virusEffect(double x){
-        if(isInfected){
-            health = health - x;
-        }
-        if(health <= 0){
-            canInfect =false;
-            isDead = true;
-        }
-    }
-
-}*/
--- src/Virus.java
+++ src/Virus.java
@@ -1,50 +1,50 @@
-//
-//import java.util.Random;
-//
-//public class Virus {
-//    int contagiousness = 1, resistance, deathRate, fieldOfView;
-//    Random rand = new Random();
-//    CombinedVisualizer visualizer = new CombinedVisualizer();
-//    public Virus(CombinedVisualizer visualizer) {
-//        this.visualizer = visualizer;
-//    }
-//    void PatientZero() {
-//        int zero = rand.nextInt(visualizer.getPopulation().size());
-//        visualizer.getPopulation().get(zero).giveSick();
-//        visualizer.getPopulation().get(zero).canInfect = true;
-//    }
-//
-//    void virusSpread() {
-//        for (int x = 0; x < visualizer.WIDTH; x++) {
-//            for (int y = 0; y < visualizer.HEIGHT; y++) {
-//                if (visualizer.getBoard()[x][y].isLand){
-//                    boolean infection = false;
-//                    for (int i = 0; i <= visualizer.getBoard()[x][y].getPeople().size(); i++) {
-//                        if (!infection) {
-//                            for (int k = -1; k <= 1; k = k + 2) {
-//                                if (visualizer.getBoard()[x][y].getPeople().get(i).canInfect || (visualizer.getBoard()[x + k][y].getPeople().get(i).canInfect || visualizer.getBoard()[x][y + k].getPeople().get(i).canInfect)) {
-//                                    infection = true;
-//                                    break;
-//                                }
-//                            }
-//                        }
-//                        if (infection) {
-//                            for (int z = 0; z <= visualizer.getBoard()[x][y].getPeople().size(); z++) {
-//
-//                                if (contagiousness + rand.nextDouble() > Human.getImmunity()) {
-//                                    visualizer.getBoard()[x][y].getPeople().get(z).giveSick();
-//                                }
-//                            }
-//                        }
-//                        infection = false;
-//                    }
-//                }
-//            }
-//        }
-//        for (Human human : visualizer.getPopulation()) {
-//            if (human.isInfected) {
-//                human.setCanInfect();
-//            }
-//        }
-//    }
-//}
+
+import java.util.Random;
+
+public class Virus {
+//    int  resistance, fieldOfView;
+    double contagiousness = 0.2, deathRate = 0.15;
+    Random rand = new Random();
+    CombinedVisualizer visualizer;
+    public Virus(CombinedVisualizer visualizer) {
+        this.visualizer = visualizer;
+    }
+    void patientZero() {
+        int zero = rand.nextInt(visualizer.getPopulation().size());
+        visualizer.getPopulation().get(zero).giveSick();
+        visualizer.getPopulation().get(zero).setCanInfect();
+    }
+
+    void virusSpread() {
+        for (int x = 0; x < visualizer.WIDTH; x++) {
+            for (int y = 0; y < visualizer.HEIGHT; y++) {
+                if (visualizer.getBoard()[x][y].isLand && !visualizer.getBoard()[x][y].getPeople().isEmpty()){
+                    for (int i = 0; i < visualizer.getBoard()[x][y].getPeople().size(); i++) {
+                                if (visualizer.getBoard()[x][y].getPeople().get(i).getCanInfect()) {
+                                    for (int z = 0; z < visualizer.getBoard()[x][y].getPeople().size(); z++) {
+
+                                        if (contagiousness + rand.nextDouble() > visualizer.getBoard()[x][y].getPeople().get(z).getImmunity()) {
+                                            visualizer.getBoard()[x][y].getPeople().get(z).giveSick();
+                                        }
+                                    }
+                                }
+                        }
+                    }
+
+
+        }
+        for(Human human : visualizer.getPopulation()) {
+            if (human.getIsInfected()) {
+                human.setCanInfect();
+            }}}
+        }
+
+    public void virusAction() {
+        for (Human human : visualizer.getPopulation()) {
+            if (human.getIsInfected()) {
+                human.virusEffect(deathRate);
+            }
+        }
+
+    }
+}
