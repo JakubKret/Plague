ba574f20cbe3549226c48a91edb69cfd359f8e20
WojszczykMaciej <wojszczykmaciej@gmail.com>
2024-06-10 18:32:53 +0200

    Update Human.java

--- src/Human.java
+++ src/Human.java
@@ -1,31 +1,46 @@
+package org.example;
+import org.example.*;
 import java.util.Random;
-
 public class Human {
-    private int posX,posY;
+    static Random rand = new Random();
     private int health = 40;
-    public Human(int X,int Y){
+    private static double immunity= rand.nextDouble();
+    public static int posX;
+    public static int posY;
+    public boolean isInfected = false;
+    public boolean canInfect = false;
+
+    public Human(int X, int Y){
         this.posX = X;
         this.posY = Y;
     }
-
     public void Move(){
-        Random rand = new Random();
+
         int x = rand.nextInt(3);
         x=x-1;
-        if(x==-1 && posX!=0)--posX;
-        else if(x==1 && posX!=CombinedVisualizer.WIDTH-1)++posX;
+        if(x==-1 && CombinedVisualizer.getLand(posX-1, posY))--posX;
+        else if(x==1 && CombinedVisualizer.getLand(posX+1, posY))++posX;
         int y = rand.nextInt(3);
         y=y-1;
-        if(y==-1 && posY!=0)--posY;
-        else if(y==1 && posY!=CombinedVisualizer.HEIGHT-1)++posY;
+        if(y==-1 && CombinedVisualizer.getLand(posX, posY-1))--posY;
+        else if(y==1 && CombinedVisualizer.getLand(posX, posY+1))++posY;
     }
     public int getHealth() {
         return health;
     }
-    public int getPosX(){
+    public static int getPosX(){
         return posX;
     }
-    public int getPosY(){
+    public static int getPosY(){
         return posY;
+    }
+    public void getSick(){
+        this.isInfected = true;
+    }
+    public void startToInfect(){
+        this.canInfect = true;
+    }
+    public static double getImmunity(){
+        return immunity;
     }
 }
