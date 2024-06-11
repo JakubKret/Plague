d1933b7fe872b717a34394910d2f87f397ee5d9a
JakubKret <57018767+JakubKret@users.noreply.github.com>
2024-06-11 11:31:10 +0200

    Update Tile.java

--- src/Tile.java
+++ src/Tile.java
@@ -3,23 +3,31 @@ import java.util.List;
 
 public class Tile {
     boolean isAirport=false;
+    ArrayList<Plane> planes =  new ArrayList<>();
     boolean isLand=false;
     private int landModifier = 100;
     public List<Human> humans = new ArrayList<Human>();
-    public Tile(int rgbVal){
+    int posX,posY;
+    public Tile(int rgbVal,int x,int y){
       if (rgbVal <= landModifier)
       {
           isLand = true;
       }
+      this.posX=x;
+      this.posY=y;
     }
     public Tile(){
         isLand = false;
+
     }
     public List<Human> getPeople(){
         return humans;
     }
-    public void createAirport()
+    public void setAirport()
     {
         isAirport = true;
+    }
+    public ArrayList<Plane> getPlanes(){
+        return planes;
     }
 }
